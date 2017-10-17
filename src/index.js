924b28f95e7239a6d311cbef78eafda076c241f9
bruno78 <devbruno78@gmail.com>
2017-10-17 16:31:24 -0400

    Add React-redux and Provider

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import C from './constants'
 import React from 'react'
 import { render } from 'react-dom'
 import routes from './routes'
+import storeFactory from './store'
 import sampleData from './initialState'
+import { Provider } from 'react-redux'
 
 const initialState = (localStorage["redux-store"]) ?
     JSON.parse(localStorage["redux-store"]) :
     sampleData
 
-const saveState = () => 
+const saveState = () =>
     localStorage["redux-store"] = JSON.stringify(store.getState())
 
-window.React = React
+
+const store = storeFactory(initialState)
+store.subscribe(saveState)
+
+// Development purposes ONLY!!!
+// window.React = React
+// window.store = store
 
 render(
-	routes,
+  <Provider store={store}>
+    {routes}
+  <Provider />,
   document.getElementById('react-container')
 )
